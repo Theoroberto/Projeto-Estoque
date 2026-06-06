ebd2d49f7ac0d63d62f439a463ad48e392d2b9f8
Theoroberto <theorobertoos@hotmail.com>
2026-06-06 16:15:49 -0300

    Primeiro commit: Projeto Base de estoque mecânico

---
+++ backend/logica.py
@@ -0,0 +1,129 @@
+import sqlite3
+from pathlib import Path
+from typing import List, Tuple, Dict
+
+ROOT_PATH = Path(__file__).parent.parent
+DATABASE_PATH = ROOT_PATH / "database"
+
+# Criação da tabela
+def conectar():
+    return sqlite3.connect(DATABASE_PATH / "estoque.db")
+
+def criar_tabela():
+    conexao = conectar()
+    try:
+        
+        with conexao:
+            cursor = conexao.cursor()
+            
+            cursor.execute("""CREATE TABLE IF NOT EXISTS estoque(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                nome TEXT NOT NULL UNIQUE,
+                preco FLOAT NOT NULL,
+                quantidade POSITIVE INTEGER NOT NULL
+                )""")
+    except Exception as exc:
+        raise exc
+
+# Create
+def inserir_produtos(data: List[Tuple]) -> None:
+    conexao = conectar()
+    try:
+        
+        with conexao:
+            cursor = conexao.cursor()
+            
+            cursor.executemany("INSERT INTO estoque (nome, preco, quantidade) VALUES (?, ?, ?)", data)
+    except Exception as exc:
+        raise exc
+    finally:
+        conexao.close()
+
+# Read
+def exibir_produtos() -> List[Dict]:
+    conexao = conectar()
+    try:
+        with conexao:
+            cursor = conexao.cursor()
+            
+            cursor.execute("SELECT id, nome, preco, quantidade FROM estoque ORDER BY nome")
+            linhas = cursor.fetchall()
+            
+            estoque = []
+            for linha in linhas:
+                estoque.append(
+                    {
+                        "id": linha[0],
+                        "nome": linha[1],
+                        "preco": linha[2],
+                        "quantidade": linha[3]
+                    }
+                )
+            return estoque
+        
+    except Exception as exc:
+        raise exc
+
+# Update
+def atualizar_preco(id: int, preco: float):
+    conexao = conectar()
+    try:
+        with conexao:
+            cursor = conexao.cursor()
+            
+            cursor.execute("UPDATE estoque SET preco = ? WHERE id = ?", (preco, id))
+    except Exception as exc:
+        raise exc
+        
+def atualizar_quantidade(id: int, quantidade: int):
+    conexao = conectar()
+    try:
+        with conexao:
+            cursor = conexao.cursor()
+
+            cursor.execute("UPDATE estoque SET quantidade = ? WHERE id = ?", (quantidade, id))
+    except Exception as exc:
+        raise exc
+
+# Função de compra
+def comprar_produto(nome: str, quantidade_comprada: int):
+    conexao = conectar()
+    try:
+        with conexao:
+            cursor = conexao.cursor()
+            
+            # Obtendo id e guardando em variável
+            cursor.execute("SELECT id FROM estoque WHERE nome = ?", (nome,))
+            produto = cursor.fetchone()
+            
+            # Verificando se o prodtuo está no estoque
+            if not produto:
+                raise ValueError("Produto não encontrado")
+            
+            # Correção 2: Usando id do produto para atualizar quantidade
+            id_produto = produto[0]
+            cursor.execute("SELECT quantidade FROM estoque WHERE nome = ?", (nome,))
+            quantidade_anterior = cursor.fetchone()[0]
+
+            # Verificando se quantidade é suficiente
+            if quantidade_anterior < quantidade_comprada:
+                raise ValueError("Quantidade insuficiente em estoque")
+
+            # Atualizando quantidade
+            quantidade_nova = quantidade_anterior - quantidade_comprada
+            cursor.execute("UPDATE estoque SET quantidade = ? WHERE id = ?", (quantidade_nova, id_produto))
+    except Exception as exc:
+        raise exc
+        
+# Delete
+def deletar_produto(id: int):
+    conexao = conectar()
+    try:
+        with conexao:
+            cursor = conexao.cursor()
+
+            cursor.execute("DELETE FROM estoque WHERE id = ?", (id,))
+    except Exception as exc:
+        raise exc
+        
+
---
+++ backend/main.py
@@ -0,0 +1,81 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import List
+from logica import exibir_produtos, atualizar_preco, atualizar_quantidade, inserir_produtos, deletar_produto, comprar_produto
+
+# Conexao do app
+app = FastAPI()
+
+# Schemas
+class ProdutoSchema(BaseModel):
+    nome: str
+    preco: float
+    quantidade: int
+    
+class PrecoSchema(BaseModel):
+    id:int
+    preco: float
+    
+class QuantidadeSchema(BaseModel):
+    id: int
+    quantidade: int
+
+class CompraSchema(BaseModel):
+    nome: str
+    quantidade_comprada: int
+
+# ROTAS
+
+# POST / main/inserir_produtos_em_lote
+@app.post("/main/inserir_produtos/lote")
+def inserir_produtos_em_lote(dados: List[ProdutoSchema]):
+    try:
+        dados_formatados = [
+            (dado.nome, dado.preco, dado.quantidade) for dado  in dados
+        ]
+        inserir_produtos(dados_formatados)
+        return {"mensagem": f"{len(dados_formatados)} produtos inseridos com sucesso!"}
+    except Exception as exc:
+        raise HTTPException(status_code=500, detail=f"Erro ao inserir produtos: {exc}")
+
+# GET / estoque
+@app.get("/main/estoque")
+def exibir_estoque():
+    return exibir_produtos()
+
+# PATCH / main/atualizar_preco/{id}
+@app.patch("/main/atualizar_preco/{id}")
+def atualizar_preco_por_id(dados: PrecoSchema):
+    try:
+        atualizar_preco(dados.id, dados.preco)
+        return {"mensagem": "Preço atualizado com sucesso!"}
+    except Exception as exc:
+        raise HTTPException(status_code=500, detail= f"Erro ao atualizar preço {exc}")
+
+# PATCH / main/atualizar_quantidade/{id}
+@app.patch("/main/atualizar_quantidade/{id}")
+def atualizar_quantidade_por_id(dados: QuantidadeSchema):
+    try:
+        atualizar_quantidade(dados.id, dados.quantidade)
+        return {"mensagem": "Quantidade atualizada com sucesso!"}
+    except Exception as exc:
+        raise HTTPException(status_code=500, detail=f"Erro ao atualizar quantidade {exc}")
+    
+# PATCH / main/comprar_produto/{nome}
+@app.patch("/main/comprar_produto/{nome}")
+def comprar_produto_por_nome(dados: CompraSchema):
+    try:
+        comprar_produto(dados.nome, dados.quantidade_comprada)
+        return {"mensagem": "Compra realizada com sucesso"}
+    except Exception as exc:
+        raise HTTPException(status_code=500, detail=f"Erro ao comprar produto {exc}")
+
+# DELETE / main/deletar_produto/{id}
+@app.delete("/main/deletar_produto/{id}")
+def deletar_produto_por_id(id: int):
+    try:
+        deletar_produto(id)
+        return {"mensagem": f"Produto com id {id} deletado com sucesso!"}
+    except Exception as exc:
+        raise HTTPException(status_code=500, detail=f"Erro ao deletar produto: {exc}")
+    
